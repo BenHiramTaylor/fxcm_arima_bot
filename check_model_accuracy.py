e80f7b5d48dc6c1f7f52e9f948e426a0a3244651
Ben Taylor <63203762+BenHiramTaylor@users.noreply.github.com>
2020-09-24 20:54:44 +0100

    fixed profit percentage

--- check_model_accuracy.py
+++ check_model_accuracy.py
@@ -4,8 +4,8 @@ import os
 if __name__ == "__main__":
     with open('APISettings.json','r') as f:
         config = json.load(f)
-    interval = config["Interval"]
-    ticker = config["Ticker"]
+    interval = config["interval"]
+    ticker = config["ticker"].replace("/","")
     if not os.path.exists(f"JSON\\{ticker}_{interval}_trade_log.json"):
         print(f"There is no saved data to analyse with the ticker: {ticker} at the interval {interval}.")
     else:
@@ -52,6 +52,10 @@ if __name__ == "__main__":
             taken_percentage = len(correct_trades_taken)/len(trades_taken)*100
         else:
             taken_percentage = 0
-        profit_percentage = len(could_have_taken_correct)/len(could_have_taken)*100
+        if len(could_have_taken):
+            profit_percentage = len(could_have_taken_correct)/len(could_have_taken)*100
+        else:
+            profit_percentage = 0
+
         print(f"Total number of correct predictions {len(correct_predictions)}/{len(total_predictions)} This is an overall accuracy of {prediction_percentage}%\nOut of this amount {len(trades_taken)} were taken and {len(correct_trades_taken)} of those were correct, this is an actual accuracy of {taken_percentage}%.\nOut of {len(total_predictions)} predictions, {len(could_have_taken)} trades could have been taken.\nOut of that amount, {len(could_have_taken_correct)} would have been profitable.\nThat is a possible profitability percentage of {profit_percentage}%")
             
